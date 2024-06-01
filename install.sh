93c2753fd53ee1e5cf7706a6461067b834e76dd1
NodeCattel <160917986+nodecattel@users.noreply.github.com>
2024-06-01 15:35:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cat << "EOF"
 █▀█ █▀█ █▀▀ █▀▄▀█ █ █▄░█ █▀▀ █▀█
 █▄█ █▀▄ ██▄ █░▀░█ █ █░▀█ ██▄ █▀▄ V2
 EOF
-echo -e "Version 0.2.1 - Ore Cli installer + PMC ui"
+echo -e "Version 0.2.2 - Ore Cli installer + PMC ui"
 echo -e "Made by NodeCattel & All the credits to HardhatChad\033[0m"
 
 # Exit script if any command fails
