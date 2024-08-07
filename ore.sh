2c93b6a349d052f00a9ae0a796eb2f168e39b3f0
Your Name <you@example.com>
2024-08-07 21:59:31 +0700

    add dynamic fee prompt

--- ore.sh
+++ ore.sh
@@ -14,7 +14,7 @@ cat << "EOF"
 █▀█ █▀█ █▀▀ █▀▄▀█ █ █▄░█ █▀▀ █▀█
 █▄█ █▀▄ ██▄ █░▀░█ █ █░▀█ ██▄ █▀▄ V2
 EOF
-echo -e "Version 0.2.2 - Ore Miner"
+echo -e "Version 0.2.4 - Ore Miner"
 echo -e "Made by NodeCattel & All the credits to HardhatChad\033[0m\n"
 
 # Configuration directory and file
@@ -27,6 +27,7 @@ BASE_PRIORITY_FEE="0"  # This is the base priority fee before any presets
 DEFAULT_THREADS="1"
 DEFAULT_KEYPAIR_PATH="$HOME/.config/solana/id.json"
 DEFAULT_BUFFER_TIME="5"
+DEFAULT_DYNAMIC_FEE_MAX="500000"
 
 # Ensure the ORE directory exists
 mkdir -p "$ORE_DIR"
@@ -61,11 +62,14 @@ show_help() {
     $ECHO "Commands:"
     $ECHO "  balance    Fetch the Ore balance of an account"
     $ECHO "  benchmark  Benchmark your machine's hashrate"
+    $ECHO "  busses     Fetch the bus account balances"
     $ECHO "  claim      Claim available mining rewards use --amount <ORE AMOUNT> to partial claim"
     $ECHO "  close      Close your onchain accounts to recover rent"
+    $ECHO "  config     Fetch the program config"
     $ECHO "  mine       Start mining Ore"
     $ECHO "  rewards    Fetch the reward rate for each difficulty level"
     $ECHO "  stake      Stake ore to earn a multiplier on your mining rewards use --amount <ORE AMOUNT> to partial stake"
+    $ECHO "  upgrade    Upgrade your ORE tokens from v1 to v2"
 }
 
 # Function to fetch the balance of an account
@@ -78,6 +82,12 @@ fetch_balance() {
 benchmark() {
     $ECHO "Benchmarking hashrate..."
     ore benchmark
+}
+
+# Function to fetch the bus account balances
+fetch_busses() {
+    $ECHO "Fetching bus account balances..."
+    ore busses --rpc "$RPC" --keypair "$KEYPAIR_PATH"
 }
 
 # Function to claim available mining rewards
@@ -147,11 +157,27 @@ close_accounts() {
     ore close --rpc "$RPC" --keypair "$KEYPAIR_PATH"
 }
 
+# Function to fetch the program config
+fetch_config() {
+    $ECHO "Fetching the program config..."
+    ore config --rpc "$RPC"
+}
+
+# Function to upgrade ORE tokens from v1 to v2
+upgrade_tokens() {
+    $ECHO "Upgrading your ORE tokens from v1 to v2..."
+    ore upgrade --rpc "$RPC" --keypair "$KEYPAIR_PATH"
+}
+
 # Function to run mining operation in background
 run_mining() {
     while :; do
         $ECHO "Mining operation started. Press CTRL+C to stop."
-        ore --rpc "$RPC" --keypair "$KEYPAIR_PATH" --priority-fee "$ADJUSTED_PRIORITY_FEE" mine --threads "$THREADS" --buffer-time "$BUFFER_TIME"
+        if [[ -n "$DYNAMIC_FEE_URL" ]]; then
+            ore mine --rpc "$RPC" --keypair "$KEYPAIR_PATH" --threads "$THREADS" --dynamic-fee-url "$DYNAMIC_FEE_URL" --dynamic-fee-max "$DYNAMIC_FEE_MAX"
+        else
+            ore mine --rpc "$RPC" --keypair "$KEYPAIR_PATH" --priority-fee "$ADJUSTED_PRIORITY_FEE" --threads "$THREADS" --buffer-time "$BUFFER_TIME"
+        fi
         if [[ $? -ne 0 ]]; then
             $ECHO "\033[0;32mMining process exited with error, restarting in 3 seconds...\033[0m"
             $SLEEP 3
@@ -170,6 +196,9 @@ case "$COMMAND" in
     benchmark)
         benchmark
         ;;
+    busses)
+        fetch_busses
+        ;;
     claim)
         $ECHO "Enter wallet address to receive ORE claim (or leave empty to use the default wallet): "
         read -r input_receiver
@@ -181,11 +210,17 @@ case "$COMMAND" in
     close)
         close_accounts
         ;;
+    config)
+        fetch_config
+        ;;
     rewards)
         fetch_rewards
         ;;
     stake)
         stake_ore "$@"
+        ;;
+    upgrade)
+        upgrade_tokens
         ;;
     mine)
         # Display Ore Cli version
@@ -213,6 +248,14 @@ case "$COMMAND" in
         read -r input_buffer_time
         BUFFER_TIME="${input_buffer_time:-${BUFFER_TIME:-$DEFAULT_BUFFER_TIME}}"
 
+        $ECHO "Enter dynamic fee URL (leave empty if not using dynamic fees) (Current: ${DYNAMIC_FEE_URL:-None}): "
+        read -r input_dynamic_fee_url
+        DYNAMIC_FEE_URL="${input_dynamic_fee_url:-${DYNAMIC_FEE_URL}}"
+
+        $ECHO "Enter maximum dynamic fee (Current: ${DYNAMIC_FEE_MAX:-$DEFAULT_DYNAMIC_FEE_MAX}): "
+        read -r input_dynamic_fee_max
+        DYNAMIC_FEE_MAX="${input_dynamic_fee_max:-${DYNAMIC_FEE_MAX:-$DEFAULT_DYNAMIC_FEE_MAX}}"
+
         # Confirm and update the config file
         $ECHO "Updating configuration..."
         {
@@ -221,6 +264,8 @@ case "$COMMAND" in
             $ECHO "THREADS=$THREADS"
             $ECHO "KEYPAIR_PATH=$KEYPAIR_PATH"
             $ECHO "BUFFER_TIME=$BUFFER_TIME"
+            $ECHO "DYNAMIC_FEE_URL=$DYNAMIC_FEE_URL"
+            $ECHO "DYNAMIC_FEE_MAX=$DYNAMIC_FEE_MAX"
         } > "$CONFIG_FILE"
 
         # Generate Solana keypair if it does not exist
@@ -240,45 +285,59 @@ case "$COMMAND" in
         # Load the updated configuration
         source "$CONFIG_FILE"
 
-        # Preset selection menu
-        $ECHO "Choose your mining speed preset:"
-        $ECHO "1) Normal - Use default priority-fee setting"
-        $ECHO "2) Fast - +25% increase to the priority-fee"
-        $ECHO "3) Chad - +50% increase to the priority-fee"
+        # Skip preset selection if dynamic fee URL is provided
+        if [[ -z "$DYNAMIC_FEE_URL" ]]; then
+            # Preset selection menu
+            $ECHO "Choose your mining speed preset:"
+            $ECHO "1) Normal - Use default priority-fee setting"
+            $ECHO "2) Fast - +25% increase to the priority-fee"
+            $ECHO "3) Chad - +50% increase to the priority-fee"
 
-        while true; do
-            read -p "Enter choice [1-3]: " preset_choice
-            if [[ "$preset_choice" =~ ^[1-3]$ ]]; then
-                break
-            else
-                $ECHO "Invalid selection. Please enter a number between 1 and 3."
-            fi
-        done
+            while true; do
+                read -p "Enter choice [1-3]: " preset_choice
+                if [[ "$preset_choice" =~ ^[1-3]$ ]]; then
+                    break
+                else
+                    $ECHO "Invalid selection. Please enter a number between 1 and 3."
+                fi
+            done
 
-        case $preset_choice in
-            1)
-                CHOICE="Normal"
-                ADJUSTED_PRIORITY_FEE=$PRIORITY_FEE
-                ;;
-            2)
-                CHOICE="Fast"
-                ADJUSTED_PRIORITY_FEE=$(printf "%.0f" $(echo "$PRIORITY_FEE + $PRIORITY_FEE * 0.25" | bc))
-                ;;
-            3)
-                CHOICE="Chad"
-                ADJUSTED_PRIORITY_FEE=$(printf "%.0f" $(echo "$PRIORITY_FEE + $PRIORITY_FEE * 0.50" | bc))
-                ;;
-        esac
+            case $preset_choice in
+                1)
+                    CHOICE="Normal"
+                    ADJUSTED_PRIORITY_FEE=$PRIORITY_FEE
+                    ;;
+                2)
+                    CHOICE="Fast"
+                    ADJUSTED_PRIORITY_FEE=$(printf "%.0f" $(echo "$PRIORITY_FEE + $PRIORITY_FEE * 0.25" | bc))
+                    ;;
+                3)
+                    CHOICE="Chad"
+                    ADJUSTED_PRIORITY_FEE=$(printf "%.0f" $(echo "$PRIORITY_FEE + $PRIORITY_FEE * 0.50" | bc))
+                    ;;
+            esac
 
-        # Update config with the latest settings and wallet address
-        {
-            $ECHO "ADJUSTED_PRIORITY_FEE=${ADJUSTED_PRIORITY_FEE}"
-        } >> "$CONFIG_FILE"
+            # Update config with the latest settings and wallet address
+            {
+                $ECHO "ADJUSTED_PRIORITY_FEE=${ADJUSTED_PRIORITY_FEE}"
+            } >> "$CONFIG_FILE"
 
-        # Confirm to start mining with the wallet address on a new line
-        $ECHO -e "Selected preset: \033[1;32m${CHOICE}\033[0m"
-        $ECHO -e "Adjusted Priority Fee: \033[1;32m${ADJUSTED_PRIORITY_FEE}\033[0m"
+            # Confirm to start mining with the wallet address on a new line
+            $ECHO -e "Selected preset: \033[1;32m${CHOICE}\033[0m"
+            $ECHO -e "Adjusted Priority Fee: \033[1;32m${ADJUSTED_PRIORITY_FEE}\033[0m"
+        fi
+
         $ECHO -e "You are mining to the wallet address: \033[1;32m${WALLET_ADDRESS}\033[0m"
+
+        # Print the final command used
+        if [[ -n "$DYNAMIC_FEE_URL" ]]; then
+            final_command="ore mine --rpc \"$RPC\" --keypair \"$KEYPAIR_PATH\" --threads \"$THREADS\" --dynamic-fee-url \"$DYNAMIC_FEE_URL\" --dynamic-fee-max \"$DYNAMIC_FEE_MAX\""
+        else
+            final_command="ore mine --rpc \"$RPC\" --keypair \"$KEYPAIR_PATH\" --priority-fee \"$ADJUSTED_PRIORITY_FEE\" --threads \"$THREADS\" --buffer-time \"$BUFFER_TIME\""
+        fi
+
+        $ECHO -e "Final command: \033[1;32m$final_command\033[0m"
+
         read -p "Press any key to start mining or CTRL+C to cancel..."
 
         # Display configuration
@@ -286,10 +345,15 @@ case "$COMMAND" in
         $ECHO "Cli Version: $ore_version"
         $ECHO "RPC URL: $RPC"
         $ECHO "Keypair Path: $KEYPAIR_PATH"
-        $ECHO "Priority Fee: $ADJUSTED_PRIORITY_FEE"
         $ECHO "Threads: $THREADS"
         $ECHO "Buffer Time: $BUFFER_TIME"
         $ECHO "Wallet Address: $WALLET_ADDRESS"
+        if [[ -n "$DYNAMIC_FEE_URL" ]]; then
+            $ECHO "Dynamic Fee URL: $DYNAMIC_FEE_URL"
+            $ECHO "Maximum Dynamic Fee: $DYNAMIC_FEE_MAX"
+        else
+            $ECHO "Priority Fee: $ADJUSTED_PRIORITY_FEE"
+        fi
 
         # Run the mining operation
         run_mining
